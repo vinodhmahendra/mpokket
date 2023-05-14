574a52e9c3a187736776b0ce0bdf53ef55f4914e
vinod mahendra <vinodh.mahendra@gmail.com>
2023-05-14 07:27:10 +0000

    Learning Purpose

--- bank-threads/src/com/simplilearn/client/TestBanking.java
+++ bank-threads/src/com/simplilearn/client/TestBanking.java
@@ -2,27 +2,38 @@ package com.simplilearn.client;
 
 import com.simplilearn.errors.InsufficientBalanceException;
 import com.simplilearn.model.*;
+import com.simplilearn.tasks.TransactionTask;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TestBanking {
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
 //        Bank simplilearnBank = new Bank();
-        Customer customer;
-        Account account; // super class type
 
-        System.out.println("Creating the customer chushruth vinodh");
-        Bank.addCustomer("chushruth", "vinodh");
-        customer = Bank.getCustomer(0);
-        System.out.println("Creating his Savings Account with a 500.00 balance and 3% interest.");
-        customer.addAccount("1", new SavingsAccount(500.00, 0.03));
-        account = customer.getAccount("1");
-        System.out.println("Account creaated with balance : " + account.getBalance());
-        System.out.println("Deposit 200.00: " + account.deposit(200.00));
-        System.out.println("Withdraw 100.00: " + account.withdraw(100.00));
-        for (Transaction transaction : account.getTransactions()) {
-            System.out.println("Transaction ID: " + transaction.getTransactionId() +
-                    ", Type: " + transaction.getType() + ", Amount: " + transaction.getAmount());
+        try {
+            System.out.println("Creating the customer chushruth vinodh");
+            Bank.addCustomer("chushruth", "vinodh");
+            Customer customer1 = Bank.getCustomer(0);
+            System.out.println("Creating his Savings Account with a 500.00 balance and 3% interest.");
+            customer1.addAccount("1", new SavingsAccount(500.00, 0.03));
+
+
+//            System.out.println("Creating the customer Samarth Vishnu");
+//            Bank.addCustomer("Samarth", "Vishnu");
+//            Customer customer2 = Bank.getCustomer(1);
+//            System.out.println("Creating his Checking Account with a 500.00 balance and no overdraft protection.");
+//            customer2.addAccount("2", new CheckingAccount(500.00));
+
+            Runnable task1 = new TransactionTask(customer1.getAccount("1"));
+//            Runnable task2 = new TransactionTask(customer2.getAccount("2"));
+
+            executorService.submit(task1);
+//            executorService.submit(task2);
+        } finally {
+            executorService.shutdown();
         }
-
     }
 }
